beb535ea1910885f145576038d57b6755584bc89
hoangtv090103 <hoangtv090103@gmail.com>
2024-07-31 17:01:00 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # model-llm
+# model-llm
--- model_llm/wizards/mllm_prompt.py
+++ model_llm/wizards/mllm_prompt.py
@@ -1,46 +1,19 @@
 from odoo import models, fields, tools
 import google.generativeai as genai
 import os
+from odoo.tools.config import config
 
 from odoo.models import BaseModel
 
 
+API_KEY = config.get("genai_api")
 
-genai.configure(api_key="AIzaSyD3tsoyQfSvqodSRW-bONxMLCJoM6u77Kw")
+genai.configure(api_key=API_KEY)
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel(
+    "gemini-1.5-flash", generation_config={"response_mime_type": "application/json"}
+)
 
-DATATYPES = {
-    'int': int,
-    'bigint': int,
-    'bigserial': int,
-    'bit': int,
-    'boolean': bool,
-    'char': str,
-    'varchar': str,
-    'text': str,
-    'citext': str,
-    'date': 'datetime.date',
-    'datetime': 'datetime.datetime',
-    'decimal': float,
-    'double precision': float,
-    'float4': float,
-    'float8': float,
-    'integer': int,
-    'interval': 'datetime.timedelta',
-    'json': dict,
-    'jsonb': dict,
-    'numeric': float,
-    'real': float,
-    'serial': int,
-    'smallint': int,
-    'smallserial': int,
-    'time': 'datetime.time',
-    'timestamp': 'datetime.datetime',
-    'timestamptz': 'datetime.datetime',
-    'uuid': 'uuid.UUID',
-    'xml': str,
-}
 
 class Prompt(models.TransientModel):
     _name = "mllm.prompt"
@@ -51,58 +24,43 @@ class Prompt(models.TransientModel):
 
     def action_prompt(self):
         try:
-            prompt = f"Write Postgresql query to create a model with name: {self.model}. \nDetail: {self.prompt}"
+            log_id = self.env["mllm.prompt.log"].create({"prompt": self.prompt})
+
+            prompt = f"Please give me fields and fields' attributes of models (words separate by '.') in Odoo with format like JSON. \nDetail: {self.prompt}"
             response = model.generate_content(prompt)
+            # model_name = self.model.replace(".", "_")
 
-            # Only get the SQL query
-            response_text = response.text
-            sql_query = response_text.split("```sql")[1].split("```")[0].strip()
-
-            # Get model name
-            sql_model_name = sql_query.split("CREATE TABLE ")[1].split(" ")[0]
-            model_name = sql_model_name.replace("_", ".") 
-
-            new_model = self.env["ir.model"].create(
-                {
-                    "name": model_name,
-                    "model": model_name,
-                    "state": "manual",
-                    "access_ids": [(6, 0, [self.env.ref("base.group_user").id])],
-                }
+            # JSON to dict
+            response_text = response.text.replace("true", "True").replace(
+                "false", "False"
             )
-            
-            # Check if model exists
-            model_obj = self.env["ir.model"].search([("model", "=", model_name)])
-            if model_obj:
-                self.env["mllm.prompt.log"].create(
+            models_data = eval(response_text)
+
+            for model_name, fields in models_data.items():
+                # Check if model exists
+                model_obj = self.env["ir.model"].search([("model", "=", model_name)])
+                if model_obj:
+                    log_id.db_query = (
+                        log_id.db_query or ""
+                    ) + f"\n[Fail] Model {model_name} already exists"
+                    continue
+
+                # TODO: Fix cannot create model
+                model_obj = self.env["ir.model"].create(
                     {
-                        "prompt": self.prompt,
-                        "model": self.model,
-                        "state": "cancel",
-                        "db_query": f"[Fail] Model {model_name} already exists.",
+                        "name": self.model,
+                        "model": model_name,
+                        "state": "manual",
                     }
                 )
-                return
+                field_objs = self.env["ir.model.fields"]
 
-            field_definitions = {}
-            for line in sql_query.split("\n")[1:]:
-                if line and line not in ["(", ");"]:
-                    line = line.strip().replace('"', "")
-                    field_name = line.split(" ")[0]
-                    field_type = line.split(" ")[1]
-                    field_type = DATATYPES.get(field_type.lower(), str)
-                    field_definitions[field_name] = (field_type, None)
+                for field in fields:
+                    ttype = field.pop("type")
+                    field_objs += field_objs.create(
+                        field.update({"model_id": model_obj.id, "ttype": ttype}),
+                    )
 
-
-            # Save the prompt log
-            self.env["mllm.prompt.log"].create(
-                {
-                    "prompt": self.prompt,
-                    "model": self.model,
-                    "db_query": sql_query,
-                    "state": "done",
-                }
-            )
             return
         except Exception as e:
             self.env["mllm.prompt.log"].create(
